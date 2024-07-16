16020bfd983e5fca9b581ea20e73d22a8f165b2e
Oleksandr Borysenko <oleksandr.borysenko@telqon.io>
2024-07-16 18:34:50 +0300

    updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,26 @@
 describe('Keepin CRM Login Test', () => {
   it('should log in with valid credentials', () => {
+
+    // Креди з енвів
+    const username = Cypress.env('username');
+    const password = Cypress.env('password');
+
     // Відвідуємо сторінку входу Keepin CRM
-    cy.visit('https://keepincrm.com');
+    cy.visit('https://my.keepincrm.com/users/sign_in?locale=uk');
 
     // Вводимо логін
-    cy.get('#username').type('your-username');
+    cy.get('#user_email').type(username);
 
     // Вводимо пароль
-    cy.get('#password').type('your-password');
+    cy.get('#user_password').type(password);
 
     // Натискаємо кнопку входу
-    cy.get('#login-button').click();
+    cy.wait(1000); // Зачекайте 1 секунду (можливо, потрібно збільшити цей час)
+    cy.get('button[type="submit"]').click();
+
+    // Очікуємо, що сторінка завантажиться повністю
+    cy.contains('Loading...').should('not.exist');
+    cy.contains('Please wait...').should('not.exist');
 
     // Перевіряємо, що успішно увійшли, наприклад, шукаємо елемент, який з'являється тільки після входу
     cy.url().should('include', '/dashboard');
